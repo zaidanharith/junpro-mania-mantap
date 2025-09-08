bbbbb836a4ba51a21bfa0acc484259e4a61aea5b
Zaidan Harith <myharithzaidan@gmail.com>
2025-09-08 14:39:50 +0700

    FIX:run debug

--- BOZea/obj/Debug/net9.0-windows/BOZea.AssemblyInfo.cs
+++ BOZea/obj/Debug/net9.0-windows/BOZea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BOZea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359233c0f8ac614b88ffc21fe52253f7cb9c93a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86223347f5cb02dda3a6d7f1d5fddbe8b19c216")]
 [assembly: System.Reflection.AssemblyProductAttribute("BOZea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BOZea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
